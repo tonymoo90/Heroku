7ec1300362108ee948ebf6ba995b028ad12a0891
tonymoo90 <tonymoo90@gmail.com>
2023-11-03 15:17:50 -0500

    Update today8.js

--- today8.js
+++ today8.js
@@ -73,7 +73,7 @@ async function fetchNews() {
         const data = await response.json();
       
         const todaysDate = new Date();
-        todaysDate.setDate(todaysDate.getDate() - 10);  // Set it to yesterday
+        todaysDate.setDate(todaysDate.getDate() - 11);  // Set it to yesterday
         todaysDate.setHours(0, 0, 0, 0);  // Set hours, minutes, seconds and milliseconds to 0
     
         // Filter articles that are published today
